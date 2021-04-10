0a4e6a949231f98861470c5b225d9ca8ccdda720
ares <47794131+ares-avi@users.noreply.github.com>
2021-04-10 17:36:05 +0400

    Update index.php

--- installation/pages/index.php
+++ installation/pages/index.php
@@ -1,11 +1,1 @@
 <?php
-/**
- * Open Source Social Network
- *
- * @package   (softlab24.com).el
- * @author    el Core Team <info@softlab24.com>
- * @copyright (C) SOFTLAB24 LIMITED
- * @license   Open Source Social Network License (el LICENSE)  http://www.opensource-socialnetwork.org/licence
- * @link      https://www.opensource-socialnetwork.org/
- */
- 
